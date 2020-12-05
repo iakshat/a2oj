106a57c22b94922539e8fdd8865f98e049a9a640
iakshat <akshat1118@gmail.com>
2020-12-06 00:30:10 +0530

    adding clutter

---
+++ precache-manifest.cada140d99f8dcef8c00f01ce688bf54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d1bc1ca86b79ca95c9e46d2b988585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edd7e85bba20459468cd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "919c1e019bf9d601e346",
+    "url": "/static/js/2.2f78de10.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2f78de10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd7e85bba20459468cd",
+    "url": "/static/js/main.59509218.chunk.js"
+  },
+  {
+    "revision": "39d47c611a20b26e8983",
+    "url": "/static/js/runtime-main.bcabb252.js"
+  }
+]);
