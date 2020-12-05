06972b3547476a85c01a216cbf10b371d68457cb
iakshat <akshat1118@gmail.com>
2020-12-06 00:07:34 +0530

    adding build

---
+++ build/precache-manifest.5c150a10db083cef71e9653e64ca28e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd2389884ccf73a3428af6bbd799c83",
+    "url": "/iakshat.github.io/a2oj/index.html"
+  },
+  {
+    "revision": "23753a0ae3cf6c3f16fc",
+    "url": "/iakshat.github.io/a2oj/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "919c1e019bf9d601e346",
+    "url": "/iakshat.github.io/a2oj/static/js/2.2f78de10.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/iakshat.github.io/a2oj/static/js/2.2f78de10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23753a0ae3cf6c3f16fc",
+    "url": "/iakshat.github.io/a2oj/static/js/main.371bc590.chunk.js"
+  },
+  {
+    "revision": "e2b54d3ed0a49d5d6f89",
+    "url": "/iakshat.github.io/a2oj/static/js/runtime-main.ed3fb4a0.js"
+  }
+]);
