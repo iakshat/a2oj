ced8f03c6dda7af3eea3557b4a74246d508ed89a
iakshat <akshat1118@gmail.com>
2020-12-06 00:32:11 +0530

    adding clutter

---
+++ precache-manifest.842967ad36e2476693c361c14bfacbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28860eff6612ba3dcdaa53b448adb633",
+    "url": "/iakshat.github.io/a2oj/index.html"
+  },
+  {
+    "revision": "175e645ffaa2db7a433b",
+    "url": "/iakshat.github.io/a2oj/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "919c1e019bf9d601e346",
+    "url": "/iakshat.github.io/a2oj/static/js/2.2f78de10.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/iakshat.github.io/a2oj/static/js/2.2f78de10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175e645ffaa2db7a433b",
+    "url": "/iakshat.github.io/a2oj/static/js/main.8d830eeb.chunk.js"
+  },
+  {
+    "revision": "e2b54d3ed0a49d5d6f89",
+    "url": "/iakshat.github.io/a2oj/static/js/runtime-main.ed3fb4a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cada140d99f8dcef8c00f01ce688bf54.js"
+  "/iakshat.github.io/a2oj/precache-manifest.842967ad36e2476693c361c14bfacbf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iakshat.github.io/a2oj/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
